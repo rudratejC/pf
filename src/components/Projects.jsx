b8cc2cdee1c0b91a7bad2a9c06950d1d5a9ee488
Arun Chilkewar <arunchilkewar@Aruns-MacBook-Air.local>
2023-11-13 13:38:25 +0530

    updated the latest

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,7 +28,7 @@ function Projects() {
 
             <div className="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 ">
               <h3 className="py-2 font-bold tracking-wide text-center text-gray-800 uppercase ">
-                Undoubt
+                Un-doubt
               </h3>
 
               <div className="flex items-center justify-evenly flex-wrap px-3 py-2 bg-gray-400">
